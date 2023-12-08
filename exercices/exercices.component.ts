e79eb613e21fb82890f114b5ae21b7b1b9906b75
cherif-ichrak <ichechri@gmail.com>
2023-12-08 10:33:34 +0100

    Update Verecl Cotisation

--- exercices/exercices.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-exercices",
-  templateUrl: "./exercices.component.html",
-  styleUrls: ["./exercices.component.css"],
-})
-export class ExercicesComponent {}
